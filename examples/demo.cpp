6415ebd8ec01cbb80dab6e79cf3d0e929a1dc14f
Alexander Cuevas <alec280@gmail.com>
2022-03-24 22:59:56 -0300

    Update demo.cpp

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -16,6 +16,11 @@ struct Controller
     float cameraY = 10;
     float playerPosX = 0;
     float playerPosY = 0;
+    float enemyPosX = 10;
+    float enemyPosY = 0;
+    bool playerMoved = false;
+    int enemyStep = 0;
+    const float enemyRoute[8][2]{{10, 0}, {11, 0}, {11, 1}, {10, 1}, {9, 1}, {9, 0}, {9, -1}, {10, -1}};
 } controller;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -31,46 +36,18 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     } else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
         controller.cameraX -= 1;
     } else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        controller.playerMoved = true;
         controller.playerPosY -= 1;
     } else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        controller.playerMoved = true;
         controller.playerPosY += 1;
     } else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        controller.playerMoved = true;
         controller.playerPosX += 1;
     } else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        controller.playerMoved = true;
         controller.playerPosX -= 1;
     }
-}
-
-template <typename PipelineT>
-gr::SceneGraphNodePtr createCar(const PipelineT& pipeline, gr::Coord r, gr::Coord g, gr::Coord b)
-{
-    // only gpu shapes created
-    auto gpuChasisPtr = std::make_shared<gr::GPUShape>(gr::toGPUShape(pipeline, gr::createColorCube(r,g,b)));
-    auto gpuWeelPtr = std::make_shared<gr::GPUShape>(gr::toGPUShape(pipeline, gr::createColorCube(0,0,0)));
-
-    // The wheel object
-    auto wheelPtr = std::make_shared<gr::SceneGraphNode>("wheel", tr::scale(0.2, 0.8, 0.2), gpuWeelPtr);
-
-    // A node to control wheel rotations
-    auto wheelRotationPtr = std::make_shared<gr::SceneGraphNode>("wheelRotation");
-    wheelRotationPtr->childs.push_back(wheelPtr);
-
-    // creating wheels
-    auto frontWheelPtr = std::make_shared<gr::SceneGraphNode>("frontWheel", tr::translate(0.3,0,-0.3));
-    frontWheelPtr->childs.push_back(wheelRotationPtr);
-
-    auto backWheelPtr = std::make_shared<gr::SceneGraphNode>("backWheel", tr::translate(-0.3,0,-0.3));
-    backWheelPtr->childs.push_back(wheelRotationPtr);
-
-    // Creating the chasis of the car
-    auto chasisPtr = std::make_shared<gr::SceneGraphNode>("chasis", tr::scale(1,0.7,0.5), gpuChasisPtr);
-
-    auto carPtr = std::make_shared<gr::SceneGraphNode>("car");
-    carPtr->childs.push_back(chasisPtr);
-    carPtr->childs.push_back(frontWheelPtr);
-    carPtr->childs.push_back(backWheelPtr);
-
-    return carPtr;
 }
 
 template <typename PipelineT>
@@ -82,10 +59,57 @@ gr::SceneGraphNodePtr createWall(const PipelineT& pipeline)
 }
 
 template <typename PipelineT>
+gr::SceneGraphNodePtr createRoom(const PipelineT& pipeline)
+{
+    gr::SceneGraphNodePtr upperWallPtr = createWall(pipeline);
+    upperWallPtr->transform = tr::scale(8, 1, 1) * tr::translate(0, -3.5, 0);
+    gr::SceneGraphNodePtr leftWallPtr = createWall(pipeline);
+    leftWallPtr->transform = tr::scale(1, 8, 1) * tr::translate(-3.5, 0, 0);
+    gr::SceneGraphNodePtr rightWallPtr = createWall(pipeline);
+    rightWallPtr->transform = tr::scale(1, 8, 1) * tr::translate(3.5, 0, 0);
+    gr::SceneGraphNodePtr bottomWallRightPtr = createWall(pipeline);
+    bottomWallRightPtr->transform = tr::scale(2.5, 1, 1) * tr::translate(1, 3.5, 0);
+    gr::SceneGraphNodePtr bottomWallLeftPtr = createWall(pipeline);
+    bottomWallLeftPtr->transform = tr::scale(2.5, 1, 1) * tr::translate(-1, 3.5, 0);
+
+    auto roomPtr = std::make_shared<gr::SceneGraphNode>("room");
+    roomPtr->childs.push_back(upperWallPtr);
+    roomPtr->childs.push_back(leftWallPtr);
+    roomPtr->childs.push_back(rightWallPtr);
+    roomPtr->childs.push_back(bottomWallRightPtr);
+    roomPtr->childs.push_back(bottomWallLeftPtr);
+
+    return roomPtr;
+}
+
+template <typename PipelineT>
+gr::SceneGraphNodePtr createChest(const PipelineT& pipeline)
+{
+    auto gpuBodyPtr = std::make_shared<gr::GPUShape>(gr::toGPUShape(pipeline, gr::createColorCube(0.5,0.3,0)));
+    auto gpuLockPtr = std::make_shared<gr::GPUShape>(gr::toGPUShape(pipeline, gr::createColorCube(0.2,0.2,0.2)));
+
+    auto lockPtr = std::make_shared<gr::SceneGraphNode>("lock", tr::uniformScale(0.3) * tr::translate(0, 1, 0), gpuLockPtr);
+    auto bodyPtr = std::make_shared<gr::SceneGraphNode>("body", tr::uniformScale(0.7), gpuBodyPtr);
+    auto chestPtr = std::make_shared<gr::SceneGraphNode>("chest");
+    chestPtr->childs.push_back(bodyPtr);
+    chestPtr->childs.push_back(lockPtr);
+
+    return chestPtr;
+}
+
+template <typename PipelineT>
 gr::SceneGraphNodePtr createPlayer(const PipelineT& pipeline)
 {
     auto gpuPlayerPtr = std::make_shared<gr::GPUShape>(gr::toGPUShape(pipeline, gr::createColorCube(0,1,0)));
     auto playerPtr = std::make_shared<gr::SceneGraphNode>("player", tr::uniformScale(0.7), gpuPlayerPtr);
+    return playerPtr;
+}
+
+template <typename PipelineT>
+gr::SceneGraphNodePtr createEnemy(const PipelineT& pipeline)
+{
+    auto gpuPlayerPtr = std::make_shared<gr::GPUShape>(gr::toGPUShape(pipeline, gr::createColorCube(1,0,0)));
+    auto playerPtr = std::make_shared<gr::SceneGraphNode>("enemy", tr::uniformScale(0.7), gpuPlayerPtr);
     return playerPtr;
 }
 
@@ -119,22 +143,29 @@ int main()
     gr::ModelViewProjectionShaderProgram pipeline;
     glUseProgram(pipeline.shaderProgram);
 
-    gr::SceneGraphNodePtr sgPlayerPtr = createPlayer(pipeline);
-    sgPlayerPtr->transform = tr::translate(controller.playerPosX, controller.playerPosY,0);
+    gr::SceneGraphNodePtr playerPtr = createPlayer(pipeline);
+    playerPtr->transform = tr::translate(controller.playerPosX, controller.playerPosY,0);
 
-    gr::SceneGraphNodePtr upperWallPtr = createWall(pipeline);
-    upperWallPtr->transform = tr::scale(8, 1, 1) * tr::translate(0, -3.5, 0);
-    gr::SceneGraphNodePtr leftWallPtr = createWall(pipeline);
-    leftWallPtr->transform = tr::scale(1, 8, 1) * tr::translate(-3.5, 0, 0);
-    gr::SceneGraphNodePtr rightWallPtr = createWall(pipeline);
-    rightWallPtr->transform = tr::scale(1, 8, 1) * tr::translate(3.5, 0, 0);
-    gr::SceneGraphNodePtr bottomWallRightPtr = createWall(pipeline);
-    bottomWallRightPtr->transform = tr::scale(2.5, 1, 1) * tr::translate(1, 3.5, 0);
-    gr::SceneGraphNodePtr bottomWallLeftPtr = createWall(pipeline);
-    bottomWallLeftPtr->transform = tr::scale(2.5, 1, 1) * tr::translate(-1, 3.5, 0);
+    gr::SceneGraphNodePtr middleRoomPtr = createRoom(pipeline);
+
+    gr::SceneGraphNodePtr leftRoomPtr = createRoom(pipeline);
+    leftRoomPtr->transform = tr::translate(-10, 0,0);
+
+    gr::SceneGraphNodePtr rightRoomPtr = createRoom(pipeline);
+    rightRoomPtr->transform = tr::translate(10, 0,0);
+
+    gr::SceneGraphNodePtr chestPtr = createChest(pipeline);
+    chestPtr->transform = tr::translate(-10, -0.5,0);
+
+    gr::SceneGraphNodePtr enemyPtr = createEnemy(pipeline);
+    enemyPtr->transform = tr::translate(
+            controller.enemyRoute[controller.enemyStep][0],
+            controller.enemyRoute[controller.enemyStep][1],
+            0);
 
     glClearColor(0, 0, 0, 1.0f);
     glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     float t0 = glfwGetTime(), t1, dt;
     float cameraTheta = M_PI / 4;
@@ -144,13 +175,8 @@ int main()
     // Application loop
     while (!glfwWindowShouldClose(window))
     {
-        // Using GLFW to check and process input events
         glfwPollEvents();
 
-        // Filling or not the shapes depending on the controller state
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        // Getting the time difference from the previous iteration
         t1 = glfwGetTime();
         dt = t1 - t0;
         t0 = t1;
@@ -170,7 +196,24 @@ int main()
 
         gr::Matrix4f view = tr::lookAt(viewPos, eye, at);
 
-        sgPlayerPtr->transform = tr::translate(controller.playerPosX, controller.playerPosY, 0);
+        playerPtr->transform = tr::translate(controller.playerPosX, controller.playerPosY, 0);
+
+        if (controller.playerMoved) {
+            controller.playerMoved = false;
+            controller.enemyStep += 1;
+            if (controller.enemyStep == 8) {
+                controller.enemyStep = 0;
+            }
+        }
+
+        enemyPtr->transform = tr::translate(
+                controller.enemyRoute[controller.enemyStep][0],
+                controller.enemyRoute[controller.enemyStep][1],
+                0);
+
+        if (controller.playerPosX == controller.enemyPosX && controller.enemyPosY == controller.playerPosY) {
+            glfwSetWindowShouldClose(window, true);
+        }
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(pipeline.shaderProgram);
@@ -178,25 +221,21 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "projection"), 1, GL_FALSE, projection.data());
         glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_FALSE, tr::identity().data());
 
-        // Getting the shape to display
-        drawSceneGraphNode(upperWallPtr, pipeline, "model");
-        drawSceneGraphNode(bottomWallLeftPtr, pipeline, "model");
-        drawSceneGraphNode(bottomWallRightPtr, pipeline, "model");
-        drawSceneGraphNode(leftWallPtr, pipeline, "model");
-        drawSceneGraphNode(rightWallPtr, pipeline, "model");
-        drawSceneGraphNode(sgPlayerPtr, pipeline, "model");
-
-        // Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
+        drawSceneGraphNode(chestPtr, pipeline, "model");
+        drawSceneGraphNode(leftRoomPtr, pipeline, "model");
+        drawSceneGraphNode(rightRoomPtr, pipeline, "model");
+        drawSceneGraphNode(middleRoomPtr, pipeline, "model");
+        drawSceneGraphNode(playerPtr, pipeline, "model");
+        drawSceneGraphNode(enemyPtr, pipeline, "model");
         glfwSwapBuffers(window);
     }
 
-    // freeing GPU memory
-    leftWallPtr->clear();
-    rightWallPtr->clear();
-    bottomWallLeftPtr->clear();
-    bottomWallRightPtr->clear();
-    upperWallPtr->clear();
-    sgPlayerPtr->clear();
+    chestPtr->clear();
+    leftRoomPtr->clear();
+    rightRoomPtr->clear();
+    middleRoomPtr->clear();
+    playerPtr->clear();
+    enemyPtr->clear();
 
     glfwTerminate();
     return 0;
