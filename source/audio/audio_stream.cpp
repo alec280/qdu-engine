5563e39859b47f414b5639586b4ccc8416adf442
Alexander Cuevas <alec280@gmail.com>
2022-06-20 00:21:24 -0400

    Add AudioStream

---
+++ source/audio/audio_stream.cpp
@@ -0,0 +1,71 @@
+#include "audio_stream.hpp"
+
+namespace QDUEngine
+{
+    drwav_uint64 AudioStream::WavData::getTotalSamples() const
+    {
+        return totalPCMFrameCount * channels;
+    }
+
+    AudioStream::AudioStream(const std::string& filename) : m_source(filename)
+    {
+        WavData audioData;
+        auto sampleData = drwav_open_file_and_read_pcm_frames_s16(filename.c_str(),
+                                                                          &audioData.channels,
+                                                                          &audioData.sampleRate,
+                                                                          &audioData.totalPCMFrameCount,
+                                                                          nullptr);
+        if (!sampleData) {
+            std::cout << "[Engine] Failed to load " << filename << " as AudioStream." << std::endl;
+            drwav_free(sampleData, nullptr);
+            m_alBufferId = 0;
+        } else if (audioData.getTotalSamples() > drwav_uint64(std::numeric_limits<size_t>::max())) {
+            std::cout << "[Engine] File " << filename << " is too big." << std::endl;
+            drwav_free(sampleData, nullptr);
+            m_alBufferId = 0;
+        } else {
+            audioData.pcmData.resize(size_t(audioData.getTotalSamples()));
+            m_totalTime = (float) audioData.totalPCMFrameCount / (float) audioData.sampleRate;
+            m_sampleRate = (int)audioData.sampleRate;
+            m_channels = (int)audioData.channels;
+            std::memcpy(audioData.pcmData.data(), sampleData, audioData.pcmData.size() * 2);
+            drwav_free(sampleData, nullptr);
+
+            //Se pasa este vector de uint16_t a OpenAL
+            OPENALCALL(alGenBuffers(1, &m_alBufferId));
+            if (m_alBufferId == 0) {
+                std::cout << "[Engine] OpenAL wasn't able to load: " << filename << std::endl;
+            }
+            OPENALCALL(alBufferData(m_alBufferId, audioData.channels > 1 ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16, audioData.pcmData.data(), audioData.pcmData.size() * 2, audioData.sampleRate));
+        }
+    }
+
+    AudioStream::~AudioStream()
+    {
+        if (m_alBufferId) {
+            OPENALCALL(alDeleteBuffers(1, &m_alBufferId));
+        }
+
+    }
+
+    void AudioStream::clear()
+    {
+        OPENALCALL(alDeleteBuffers(1, &m_alBufferId));
+        m_alBufferId = 0;
+    }
+
+    ALuint AudioStream::getBufferId() const
+    {
+        return m_alBufferId;
+    }
+
+    std::string& AudioStream::getSource()
+    {
+        return m_source;
+    }
+
+    float AudioStream::getTotalTime() const
+    {
+        return m_totalTime;
+    }
+}
---
+++ source/audio/audio_stream.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <AL/al.h>
+#include <dr_wav.h>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#define OPENALCALL(function)\
+	function;\
+	{\
+		ALenum error = alGetError(); \
+		if (error != AL_NO_ERROR) {  \
+            std::cout << "OpenAL Error" << std::endl; \
+        }}
+
+namespace QDUEngine
+{
+    class AudioStream {
+    public:
+        AudioStream() = default;
+        explicit AudioStream(const std::string& filename);
+        ~AudioStream();
+        void clear();
+        ALuint getBufferId() const;
+        std::string& getSource();
+        float getTotalTime() const;
+    private:
+        struct WavData {
+            unsigned int channels = 0;
+            unsigned int sampleRate = 0;
+            drwav_uint64 totalPCMFrameCount = 0;
+            std::vector<uint16_t> pcmData;
+            drwav_uint64 getTotalSamples() const;
+        };
+        ALuint m_alBufferId = 0;
+        int m_channels = 0;
+        int m_sampleRate = 0;
+        std::string m_source;
+        float m_totalTime = 0.f;
+    };
+}
